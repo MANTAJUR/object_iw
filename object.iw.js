e353053281a52681c17f0dbee9a3a000b4797007
unknown <zeroone.mantaj@gmail.com>
2023-02-10 02:14:31 +0530

    write object i.w

---
+++ object.iw.js
@@ -0,0 +1,11 @@
+// Given a string print, the number of times each character appears
+let str="manta";
+let obj={};
+for(let i=0;i<str.length;i++){
+  if(obj[str[i]]==undefined){
+    obj[str[i]]=1;
+  }else{
+    obj[str[i]]++;
+  }
+}console.log(obj);
+  
